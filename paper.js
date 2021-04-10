17e7f2c6031295f3cf9fe815c0a25a0a4f19326b
Atharva911 <80034672+Atharva911@users.noreply.github.com>
2021-04-10 17:52:19 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,41 @@
+class Crumbled  {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.2
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      
+      this.image=loadImage("paper.png")
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      imageMode(CENTER)
+      image(this.image,0,this.width,this.height)
+      //strokeWeight(4);
+      stroke("green");
+      fill(255);
+     // rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+  function KeyPressed(){
+  
+ if(keyCode===UP_ARROW){
+     Matter.Body.applyForce(Crumbled.body,Crumbled.position,{x:130,y:-145})
+ }
+
+
+
+
+
+}
